a6c979ea6751e55a01131862d73b354bf001948c
D0491 <dan.fenichel@yahoo.com>
2019-12-19 21:22:16 -0600

    added javascript for changing time block colors based on time of day

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 var currentDayEl = $("#currentDay");
+var saveBtnEl = $(".saveBtn");
+var textBoxEl = $(".time-block");
+
+
+currentDayEl.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
+
+var update = function () {
     currentDayEl.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-    var update = function () {
-        currentDayEl.text(moment().format('MMMM Do YYYY, h:mm:ss a'));
-    }
-    setInterval(update, 1000);
+
+    // var currHour = moment().hour();
+    var currHour = 12;
+
+    textBoxEl.each(function (index, element) {
+        element = $(element);
+        if (currHour > element.attr("data-time")) {
+            element.addClass("past").removeClass("future");
+        } else if (currHour == element.attr("data-time")) {
+            element.addClass("present").removeClass("future");
+        }
+    });
+}
+setInterval(update, 1000);
+
